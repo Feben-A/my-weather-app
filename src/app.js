343449a287681c322619dc9f8eedef24eb70dceb
Feben-A <afa508165@gmail.com>
2023-12-31 21:31:38 +0000

    Added current time details using api data
    Day, weather description, time and icon image

--- src/app.js
+++ src/app.js
@@ -5,8 +5,47 @@ function displayDetails(response) {
    h1.innerHTML = `${city}`;
 
    let currentTemp = document.querySelector("#current-temperature");
-   let cityTemp = response.data.temperature.current;
-   currentTemp.innerHTML = `${Math.round(cityTemp)}`;
+   currentTemp.innerHTML = `${Math.round(response.data.temperature.current)}`;
+
+   let currentHumidity = document.querySelector("#current-humidity");
+   currentHumidity.innerHTML = `${Math.round(response.data.temperature.humidity)}%`
+
+   let currentWind = document.querySelector("#current-wind");
+   currentWind.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
+
+   let weatherDescription = document.querySelector("#weather-description")
+   weatherDescription.innerHTML = `${response.data.condition.description}`;
+
+  let weatherIcon = document.querySelector("#weather-icon");
+  weatherIcon.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon"/>`;
+  console.log(response.data.condition.icon_url);
+
+
+  let date = new Date (response.data.time * 1000);
+  formatDate(date); }
+
+
+function formatDate (date) {
+
+ let minutes = date.getMinutes();
+ let hours = date.getHours();
+ let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+ let day = days[date.getDay()];
+
+ if (hours < 10) {
+    hours = `0${hours}`
+ }
+
+ if (minutes < 10) {
+    minutes = `0${minutes}`
+ }
+
+
+ let currentTime = document.querySelector("#current-time");
+ currentTime.innerHTML = `${day} ~ ${hours}:${minutes} ~`
+
+
+
 }
 
 function cityDetails(city) {
@@ -24,4 +63,4 @@ function searchCity(event) {
 }
 
 let form = document.querySelector("#search-city-form");
-form.addEventListener("submit", searchCity)
+form.addEventListener("submit", searchCity);
